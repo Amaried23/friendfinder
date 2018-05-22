0c42a7adbca59d6f054650bd30184bb716710422
ALEXIS DELUCIA <alexisdelucia23@gmail.com>
2018-05-22 14:15:26 -0400

    changing logic and adding all comments

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,26 @@
 // ===============================================================================
 // DATA
-var friendsArray = [
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+var friends = [
   {
-  "name":"Alexis",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ],
-    "questionSum" : "10"
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
   }
-]
+];
 
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = friendsArray;
+module.exports = friends;
